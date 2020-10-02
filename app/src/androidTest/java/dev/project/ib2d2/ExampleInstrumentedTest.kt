0f82a8e6de092b9acd8d4bc675e31f8775bf5d95
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2020-10-01 22:07:39 -0400

    Test for sign up added

--- app/src/androidTest/java/dev/project/ib2d2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/dev/project/ib2d2/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package dev.jzdevelopers.cstracker
+package dev.project.ib2d2
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.jzdevelopers.cstracker", appContext.packageName)
+        assertEquals("dev.project.ib2d2", appContext.packageName)
     }
 }
---
+++ app/src/main/java/dev/project/ib2d2/MainActivity.kt
@@ -0,0 +1,49 @@
+package dev.project.ib2d2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = javaClass.name
+    private val db = FirebaseFirestore.getInstance()
+
+    private lateinit var signUpButton: Button
+    private lateinit var usernameText: EditText
+    private lateinit var passwordText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        signUpButton = findViewById(R.id.signUp_button)
+        usernameText = findViewById(R.id.editText_username)
+        passwordText = findViewById(R.id.editText_password)
+
+        signUpButton.setOnClickListener {
+            signUp(usernameText, passwordText)
+        }
+    }
+
+    private fun signUp(username: EditText, password: EditText) {
+
+        // TODO Passwords must always be hashed before being saved, this is for testing atm
+        val testUser = hashMapOf(
+            "username" to username.text.toString(),
+            "password" to password.text.toString(),
+        )
+        db.collection("test")
+            .add(testUser)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "Document added with ID ${documentReference.id}")
+                username.setText("")
+                password.setText("")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+}
