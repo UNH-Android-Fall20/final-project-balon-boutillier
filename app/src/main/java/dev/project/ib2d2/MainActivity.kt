285acd195d25793e9a42bb901a0c8498f624bfd4
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2020-10-18 18:30:58 -0400

    Layout for register screen added

--- app/src/main/java/dev/project/ib2d2/MainActivity.kt
+++ app/src/main/java/dev/project/ib2d2/MainActivity.kt
@@ -11,27 +11,43 @@ class MainActivity : AppCompatActivity() {
     private val TAG = javaClass.name
     private val db = FirebaseFirestore.getInstance()
 
-    private lateinit var signUpButton: Button
+    private lateinit var loginButton: Button
     private lateinit var usernameText: EditText
     private lateinit var passwordText: EditText
+    private lateinit var createAccountButton: Button
+    private lateinit var signUpButton: Button
+    private lateinit var confirmPasswordText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        signUpButton = findViewById(R.id.signUp_button)
+        loginButton = findViewById(R.id.login_button)
         usernameText = findViewById(R.id.editText_username)
         passwordText = findViewById(R.id.editText_password)
+        createAccountButton = findViewById(R.id.createAccount_button)
 
-        signUpButton.setOnClickListener {
-            signUp(usernameText, passwordText)
+        createAccountButton.setOnClickListener {
+            setContentView(R.layout.register_layout)
+
+            // TODO function to initialize the components in each layout
+            signUpButton = findViewById(R.id.signUp_button)
+            confirmPasswordText = findViewById(R.id.editText_confirmPassword)
+            usernameText = findViewById(R.id.editText_username)
+            passwordText = findViewById(R.id.editText_password)
+
+            signUpButton.setOnClickListener {
+                if(confirmPasswordText.text.toString() == passwordText.text.toString()) {
+                    signUp(usernameText, passwordText, confirmPasswordText)
+                }
+            }
         }
     }
 
-    private fun signUp(username: EditText, password: EditText) {
+    private fun signUp(username: EditText, password: EditText, confirmPassword: EditText) {
 
         // TODO Passwords must always be hashed before being saved, this is for testing atm
-        if(username.text.toString() != "" && password.text.toString() != "") {
+        if(username.text.toString() != "" && password.text.toString() != "" ) {
             val testUser = hashMapOf(
                 "username" to username.text.toString(),
                 "password" to password.text.toString(),
@@ -42,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "Document added with ID ${documentReference.id}")
                     username.setText("")
                     password.setText("")
+                    confirmPassword.setText("")
                 }
                 .addOnFailureListener { e ->
                     Log.w(TAG, "Error adding document", e)
